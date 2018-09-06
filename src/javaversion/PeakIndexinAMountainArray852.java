f8d7a995fde475523fd07f9fc7b1dda261ed47b9
jp <690320450@qq.com>
2018-09-06 18:21:45 +0800

    876

--- src/javaversion/PeakIndexinAMountainArray852.java
+++ src/javaversion/PeakIndexinAMountainArray852.java
@@ -1,6 +1,7 @@
 package javaversion;
 
 /**
+ * 852. 山脉数组的峰顶索引
  * 我们把符合下列属性的数组 A 称作山脉：
  * A.length >= 3
  * 存在 0 < i < A.length - 1 使得A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
