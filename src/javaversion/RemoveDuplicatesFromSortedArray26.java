fd27bec114f82b4572c76923ee55e091cf739942
jp <690320450@qq.com>
2018-09-06 18:43:10 +0800

    26

--- src/javaversion/RemoveDuplicatesFromSortedArray26.java
+++ src/javaversion/RemoveDuplicatesFromSortedArray26.java
@@ -9,7 +9,7 @@ package javaversion;
  * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
  * 你不需要考虑数组中超出新长度后面的元素。
  **/
-public class RemoveDuplicatesFromSortedArray {
+public class RemoveDuplicatesFromSortedArray26 {
 
     public static void main(String[] args) throws Exception {
         int[] a = {1, 1, 2};
